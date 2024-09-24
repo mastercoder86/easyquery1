7b4a5fa6a83d3cd4e9982244d179e1c749308bb9
mastercoder86 <budhakumar21@gmail.com>
2024-09-24 12:35:34 +0530

    new commit

---
+++ src/main/java/com/shopping_centre/ShoppingCentre1Application.java
@@ -0,0 +1,13 @@
+package com.shopping_centre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCentre1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCentre1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopping_centre/ShoppingCentre1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping_centre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCentre1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
